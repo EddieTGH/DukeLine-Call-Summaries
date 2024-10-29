3f4bda9c93147963b2d9ad427abee7c352129ad3
EddieTGH <edmond.niu@gmail.com>
2024-10-29 16:43:19 -0400

    deleted all template files

--- ourapp/railsapp/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    
-    has_many :friendships
-    has_many :friends, through: :friendships, source: :friend
-
-    validates :email, presence: true, uniqueness: true
-end
