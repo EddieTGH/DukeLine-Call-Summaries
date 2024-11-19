1b9a3cc5f981e9a0568abf446e4acef59e4f3477
EddieTGH <edmond.niu@gmail.com>
2024-11-19 17:49:30 -0500

    Prepared for deployment

--- ourapp/railsapp/app/controllers/application_controller.rb
+++ ourapp/railsapp/app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
-# app/controllers/application_controller.rb
-
-require 'rack_authenticator'
-
 class ApplicationController < ActionController::Base
-  # Skip CSRF verification for API requests
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token # Not recommended for production!
 
-  # Set CORS headers for all requests
-  before_action :set_cors_headers
-
-  # Method to set CORS headers
-  def set_cors_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
 end
